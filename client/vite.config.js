a1586def97b8a5996953e97e55d3fc3cdb4c0724
ashoknarayansmn <ashoknarayansmn@gmail.com>
2026-07-19 11:37:44 +0530

    Configure GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Jeevadhwani/",
   server: {
     proxy: {
       "/api": "http://localhost:5000",
